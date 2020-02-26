24dedbc9fa9b7b8d26b18128e1e1102d5945b0f5
b9058369 <b9058369@newcastle.ac.uk>
2020-02-26 20:21:54 +0000

    added Stock Class

---
+++ src/main/java/csc1035/project3/Stock.java
@@ -0,0 +1,29 @@
+import javax.persistence.*;
+
+
+
+public class Stock{
+    @Entity
+    @Table(name="STOCK")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cid", updatea)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
