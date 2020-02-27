b1ec4c9edabda433b52fb2520ca1ac0eed4dde7e
b9013867 <b.a.thompson-watson1@newcastle.ac.uk>
2020-02-27 14:09:42 +0000

    Added main file

---
+++ src/main/java/csc1035/project3/Main.java
@@ -0,0 +1,20 @@
+package main.java.csc1035.project3;
+
+import org.hibernate.Session;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Stock s1 = new Stock(1,"Gold Ring","Jewellery",false,120.00,4,180.00);
+
+        Session session = csc1035.project3.HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(s1);
+
+        session.getTransaction().commit();
+
+        session.close();
+    }
+
+}
