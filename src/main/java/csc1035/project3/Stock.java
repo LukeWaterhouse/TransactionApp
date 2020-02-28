bd970dcf00d1694a33bde9dec6c07db5f874723c
b9013867 <b.a.thompson-watson1@newcastle.ac.uk>
2020-02-28 15:21:37 +0000

    Revert "Stock finished"

--- src/main/java/csc1035/project3/Stock.java
+++ src/main/java/csc1035/project3/Stock.java
@@ -1,34 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-package csc1035.project3;
-
-import javax.persistence.*;
-
-
-@Entity(name = "Stock")
-=======
-=======
-/**
- * @author Ben Thompson-Watson
- * @version 1.2
- * @since 28-02-2020
- */
-
-
->>>>>>> dbe664bd7e22b3f06fafc28cbc83da00306dc70f
 package main.java.csc1035.project3;
 
 import javax.persistence.*;
 
-<<<<<<< HEAD
->>>>>>> 7e718d9bb89c6bed3c08753bec6222c14401eb9c
-=======
-/**
- * Class table for stock.
- */
-
 @Entity(name = "Stock")
->>>>>>> dbe664bd7e22b3f06fafc28cbc83da00306dc70f
 public class Stock {
 
     @Id
@@ -36,14 +10,11 @@ public class Stock {
     @Column(updatable = false, nullable = false)
     private int id;
 
-    @OneToOne(mappedBy = "transaction")
-    private
-
     @Column(name = "name")
     private String name;
 
-    @Column(name = "category")
-    private String category;
+    @Column(name = "catergory")
+    private String catergory;
 
     @Column(name = "perishable")
     private boolean perishable;
@@ -57,137 +28,65 @@ public class Stock {
     @Column(name = "sell_price")
     private double sell_price;
 
-    /**
-     * Constructor for Stock when a new object is created.
-     * Initialises values for object.
-     *
-     * @param name name of the stock.
-     * @param category name of category.
-     * @param perishable is the product perishable or not.
-     * @param cost how much the product is.
-     * @param stock how much there is of this product.
-     * @param sell_price how much it is sold for in the shop.
-     */
-
-    public Stock(String name, String category, boolean perishable, double cost, int stock, double sell_price){
+    public Stock(String name, String catergory, boolean perishable, double cost, int stock, double sell_price){
 
         this.name = name;
-        this.category = category;
+        this.catergory = catergory;
         this.perishable = perishable;
         this.cost = cost;
         this.stock = stock;
         this.sell_price = sell_price;
     }
 
-    /**
-     * Gets the id of a product.
-     *
-     * @return id
-     */
     public int getId() {
         return id;
     }
 
-
-    /**
-     * Gets the name of a product.
-     *
-     * @return name
-     */
     public String getName() {
         return name;
     }
 
-
-    /**
-     * Change the name of the product
-     */
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * Gets the category of a product.
-     *
-     * @return category
-     */
-    public String getCategory() {
-        return category;
+    public String getCatergory() {
+        return catergory;
     }
 
-    /**
-     * Change the category of the product
-     */
-    public void setCategory(String category) {
-        this.category = category;
+    public void setCatergory(String catergory) {
+        this.catergory = catergory;
     }
 
-    /**
-     * Returns true or false depending on if the product
-     * is perishable.
-     *
-     * @return perishable
-     */
     public boolean isPerishable() {
         return perishable;
     }
 
-    /**
-     * Change if the product is perishable or not.
-     */
     public void setPerishable(boolean perishable) {
         this.perishable = perishable;
     }
 
-    /**
-     * Gets the cost of the product.
-     *
-     * @return cost
-     */
     public double getCost() {
         return cost;
     }
 
-    /**
-     * Change the cost of the product
-     */
     public void setCost(double cost) {
         this.cost = cost;
     }
 
-    /**
-     * Gets the quantity of stock for that product.
-     *
-     * @return stock
-     */
     public int getStock() {
         return stock;
     }
 
-    /**
-     * Change the quantity of the stock for that product.
-     */
     public void setStock(int stock) {
         this.stock = stock;
     }
 
-    /**
-     * Get the sell price of the product.
-     *
-     * @return sell_price
-     */
     public double getSell_price() {
         return sell_price;
     }
 
-    /**
-     * Change the sell_price of the product
-     */
     public void setSell_price(double sell_price) {
         this.sell_price = sell_price;
     }
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 7e718d9bb89c6bed3c08753bec6222c14401eb9c
