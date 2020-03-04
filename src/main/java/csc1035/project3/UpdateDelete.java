92044dd67cf1d85d985948f688ef31294fb24799
b8070274 <g.christodoulou1@newcastle.ac.uk>
2020-03-04 11:46:55 +0000

    Added n parameter to all methods and fixed some mistakes.

--- src/main/java/csc1035/project3/UpdateDelete.java
+++ src/main/java/csc1035/project3/UpdateDelete.java
@@ -2,32 +2,35 @@ package csc1035.project3;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class UpdateDelete implements Update_Delete{
 
 
     /**
-     * Decreases the stock value of an element by 1.
+     * Decreases the stock value of an element by n.
      *
      * @param id
+     * @param n
      */
     @Override
-    public void deleteStock(int id) {
+    public void deleteStock(int id, int n) {
         Session session = HibernateUtil.getSessionFactory().openSession
                 ();
         try {
-            session = (Session) HibernateUtil.getSessionFactory().openSession().
-                    beginTransaction();
+            session.beginTransaction();
             Stock item = session.get(Stock.class, id);
             int stock = item.getStock();
             if (stock == 1) //Checks if there is only one item remaining in the store.
             {               //If true, then the stock would reach 0, so we delete the whole column instead.
                 session.delete(item);
                 session.getTransaction().commit();
+                session.close();
             }
-            stock -= 1;
+            stock -= n;
             item.setStock(stock);
             session.update(item);
+            session.getTransaction().commit();
         } catch (HibernateException e){
             if (session!=null) session.getTransaction().rollback();
             e.printStackTrace();
@@ -42,19 +45,21 @@ public class UpdateDelete implements Update_Delete{
      * Increases the stock value of an element by 1.
      *
      * @param id
+     * @param n
      */
     @Override
-    public void addStock(int id) {
+    public void addStock(int id, int n) {
         Session session = HibernateUtil.getSessionFactory().openSession
                 ();
         try {
-            session = (Session) HibernateUtil.getSessionFactory().openSession().
-                    beginTransaction();
+            session.beginTransaction();
             Stock item = session.get(Stock.class, id);
             int stock = item.getStock();
-            stock += 1;
+            stock += n;
             item.setStock(stock);
             session.update(item);
+            session.getTransaction().commit();
+
         } catch (HibernateException e){
             if (session!=null) session.getTransaction().rollback();
             e.printStackTrace();
@@ -66,21 +71,18 @@ public class UpdateDelete implements Update_Delete{
 
 
     /**
-     * Decreases the stock value of an item by 1, caused by a transaction.
+     * Decreases the stock value of an item by n, caused by a transaction.
      *
      * @param id
+     * @param n
      */
     @Override
-    public void updatewTrans(int id) {
+    public void updatewTrans(int id, int n) {
         Session session = HibernateUtil.getSessionFactory().openSession
                 ();
         try {
-            session = (Session) HibernateUtil.getSessionFactory().openSession().
-                    beginTransaction();
-            Stock item = session.get(Stock.class, id);
-            deleteStock(id);
-
-
+            session.beginTransaction();
+            deleteStock(id, n);
 
         }catch (HibernateException e){
             if (session!=null) session.getTransaction().rollback();
@@ -99,17 +101,16 @@ public class UpdateDelete implements Update_Delete{
      * @param add_del
      */
     @Override
-    public void updatewoutTrans(int id, boolean add_del) {
+    public void updatewoutTrans(int id, boolean add_del, int n) {
 
 
         Session session = HibernateUtil.getSessionFactory().openSession
                 ();
         try {
-            session = (Session) HibernateUtil.getSessionFactory().openSession().
-                    beginTransaction();
+            session.beginTransaction();
 
-            if (add_del) addStock(id);
-            else deleteStock(id);
+            if (add_del) addStock(id, n);
+            else deleteStock(id, n);
 
         }catch (HibernateException e){
             if (session!=null) session.getTransaction().rollback();
--- src/main/java/csc1035/project3/Update_Delete.java
+++ src/main/java/csc1035/project3/Update_Delete.java
@@ -2,13 +2,13 @@ package csc1035.project3;
 
 public interface Update_Delete {
 
-    public void deleteStock(int id);
+    public void deleteStock(int id, int n) ;
 
-    public void addStock(int id);
+    public void addStock(int id, int n);
 
-    public void updatewTrans(int id);
+    public void updatewTrans(int id, int n);
 
-    public void updatewoutTrans(int id, boolean add_del);
+    public void updatewoutTrans(int id, boolean add_del, int n);
 
 
 }
